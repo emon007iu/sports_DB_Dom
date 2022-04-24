29a5c569863ab9922d5534505c05619fdc1b4ba5
emon007iu <emon007iu@gmail.com>
2022-04-24 23:41:25 +0600

    add player

--- js/app.js
+++ js/app.js
@@ -3,22 +3,55 @@ const allPlayers = () => {
     const searchcValueText = searchValue.value;
 
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchcValueText}`;
-    console.log(url);
     fetch(url)
     .then(response => response.json())
-    .then((data) => console.log(data))
+    .then((data) => showPlayerDetails(data.player))
 
 
-    searchValue.value = ''
+    // searchValue.value = ''
+}
+
+const showPlayerDetails = (players) => {
+    for(const player of players){
+        const parent = document.getElementById('player-container');
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="card border mb-4">
+                <div class="pro-pic">
+                    <img class="w-100" src="${player.strThumb}" alt="" srcset="">
+                </div>
+                <div class="card-body p-3">
+                    <h2>Name: ${player.strPlayer}</h2>
+                    <h5>Country: ${player.strNationality}</h5>
+                    <p>${player.strKit}</p>
+                    <div class="allbutton">
+                        <button class="btn btn-danger">Delete</button>
+                        <button onclick= "details('${player.idPlayer}')" class="btn btn-success ms-3"><Details></Details></button>
+                    </div>
+                </div>
+            </div> `;
+        parent.appendChild(div);
+    }
+    
 }
 
 
+const details = (id) => {
+    const url =`https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => setDetails(data.players[0]))
+}
 
 
-
-
-
-
+const setDetails = (info) => {
+    console.log(info);
+    document.getElementById('details-container').innerHTML = `
+        <div>
+            <h1>Name: ${info.strPlayer}</h1>
+        </div>
+    `;
+}
 
 
 
